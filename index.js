f8a2ae8cf23a8a13a05d7259563a5708a66580da
Joe Jamsky <cba1067950@yahoo.com>
2019-03-26 22:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(name, holiday){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(holiday, days){
+function holidayCountdown(days, holiday){
   return `It's ${days} days until ${holiday}!`;
 }
